8b580f3008ad12a015e61f9161acf2e996af68f6
Jenny Phu <phuje@cip.ifi.lmu.de>
2020-12-07 00:43:20 +0100

    add model package with User, MainActivity with two buttons: intent to LoginActivity and SignInActivity

--- app/src/main/java/com/example/virtualbreak/view/view_activitys/MainActivity.kt
+++ app/src/main/java/com/example/virtualbreak/view/view_activitys/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.virtualbreak.view.view_activitys
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.virtualbreak.R
 
 class MainActivity : AppCompatActivity() {
+    lateinit var signupBtn: Button
+    lateinit var loginBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        signupBtn = findViewById(R.id.signupButton)
+        signupBtn.setOnClickListener {
+            startActivity(Intent(this, SignInActivity::class.java))
+        }
+
+        loginBtn = findViewById(R.id.loginButton)
+        loginBtn.setOnClickListener {
+            startActivity(Intent(this, LogInActivity::class.java))
+        }
+
     }
 }

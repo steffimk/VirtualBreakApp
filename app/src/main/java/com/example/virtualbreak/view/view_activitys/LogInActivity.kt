c4dde7fd2bcc81fa1b936c41e3d301984f4d4660
Steffi <stefanie.m.kloss@t-online.de>
2020-12-07 16:54:39 +0100

    SignUp and LogIn with Mail and Password

--- app/src/main/java/com/example/virtualbreak/view/view_activitys/LogInActivity.kt
+++ app/src/main/java/com/example/virtualbreak/view/view_activitys/LogInActivity.kt
@@ -1,12 +1,82 @@
 package com.example.virtualbreak.view.view_activitys
 
-import androidx.appcompat.app.AppCompatActivity
+//import com.google.android.gms.auth.api.signin.GoogleSignIn
+//import com.google.android.gms.auth.api.signin.GoogleSignInClient
+//import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.virtualbreak.R
+import com.example.virtualbreak.databinding.ActivityLogInBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LogInActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+//    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var binding: ActivityLogInBinding
+
+    private val TAG: String = "LogInActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_log_in)
+
+        binding = ActivityLogInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginSubmit.setOnClickListener {
+            tryAndLogIn(binding.loginEmail.text.toString(), binding.loginPassword.text.toString())
+        }
+//        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//            .requestEmail()
+//            .build()
+//        googleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        auth = Firebase.auth
     }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        //TODO: updateUI(currentUser) --> currentUser is null if no sign in yet
+    }
+
+    private fun tryAndLogIn(email: String, password: String) {
+        if (email.isEmpty()) {
+            Toast.makeText(
+                baseContext, R.string.toast_enter_mail,
+                Toast.LENGTH_SHORT
+            ).show()
+            return
+        }
+        if (password.isEmpty()) {
+            Toast.makeText(
+                baseContext, R.string.toast_enter_pw,
+                Toast.LENGTH_SHORT
+            ).show()
+            return
+        }
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    // TODO: updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext, R.string.toast_loginFailed,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    // TODO: updateUI(user)
+                    // ...
+                }
+            }
+    }
+
 }
--- app/src/main/java/com/example/virtualbreak/view/view_activitys/SignInActivity.kt
+++ app/src/main/java/com/example/virtualbreak/view/view_activitys/SignInActivity.kt
@@ -1,12 +1,78 @@
 package com.example.virtualbreak.view.view_activitys
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.virtualbreak.R
+import com.example.virtualbreak.databinding.ActivitySignInBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 
 class SignInActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivitySignInBinding
+
+    private val TAG: String = "SignInActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signinSubmit.setOnClickListener {
+            tryAndSignUp(binding.singinName.text.toString(), binding.signinEmail.text.toString(),
+                    binding.signinPassword1.text.toString(), binding.signinPassword2.text.toString())
+        }
+
+        auth = Firebase.auth
     }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        //TODO: updateUI(currentUser) --> currentUser is null if no sign in yet
+    }
+
+    private fun tryAndSignUp(name: String, email: String, password1: String, password2: String) {
+        if (name.isEmpty() || email.isEmpty() || password1.isEmpty() || password2.isEmpty()) {
+            Toast.makeText(
+                    baseContext, R.string.toast_enter_data,
+                    Toast.LENGTH_SHORT
+            ).show()
+            return
+        }
+        if (password1 != password2){
+            Toast.makeText(
+                    baseContext, R.string.toast_pw_different,
+                    Toast.LENGTH_SHORT
+            ).show()
+            return
+        }
+        auth.createUserWithEmailAndPassword(email, password1)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success")
+                        val user = auth.currentUser
+                        // TODO: updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(
+                                baseContext, R.string.toast_signIn_Failed,
+                                Toast.LENGTH_SHORT
+                        ).show()
+                        // TODO: updateUI(null)
+                    }
+
+                    // ...
+                }
+    }
+
+
 }

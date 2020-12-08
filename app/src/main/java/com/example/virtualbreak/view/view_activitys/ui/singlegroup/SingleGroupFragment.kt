d9bb56340cc279595b3f2fe94cf63584107ab10f
Jenny Phu <phuje@cip.ifi.lmu.de>
2020-12-08 15:26:56 +0100

    add Navigationdrawer, Test Fragments

---
+++ app/src/main/java/com/example/virtualbreak/view/view_activitys/ui/singlegroup/SingleGroupFragment.kt
@@ -0,0 +1,31 @@
+package com.example.virtualbreak.view.view_activitys.ui.singlegroup
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.virtualbreak.R
+
+class SingleGroupFragment : Fragment() {
+
+    private lateinit var singleGroupViewModel: SingleGroupViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        singleGroupViewModel =
+            ViewModelProvider(this).get(SingleGroupViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_singlegroup, container, false)
+        val textView: TextView = root.findViewById(R.id.text_singlegroup)
+        singleGroupViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}

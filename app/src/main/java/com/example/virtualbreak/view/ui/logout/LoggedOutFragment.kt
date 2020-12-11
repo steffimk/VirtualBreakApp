459164b0bd05bad7fb53630751d1551dcaf3aee4
Jenny Phu <phuje@cip.ifi.lmu.de>
2020-12-11 22:36:53 +0100

    loggedOutFragment asks to confirm logout + logout

--- app/src/main/java/com/example/virtualbreak/view/ui/logout/LoggedOutFragment.kt
+++ app/src/main/java/com/example/virtualbreak/view/ui/logout/LoggedOutFragment.kt
@@ -1,35 +1,32 @@
 package com.example.virtualbreak.view.ui.logout
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.NavHostFragment
 import com.example.virtualbreak.R
+import com.example.virtualbreak.view.view_activitys.MainActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoggedOutFragment : Fragment() {
-
-    private lateinit var loggedOutViewModel: LoggedOutViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        loggedOutViewModel =
-            ViewModelProvider(this).get(LoggedOutViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_loggedout, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        loggedOutViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
 
-        //TODO really log out user + evtl not as Fragment but new Activity
+        val root = inflater.inflate(R.layout.fragment_loggedout, container, false)
+
+        root.findViewById<Button>(R.id.logoutBtn).setOnClickListener {
+            //Sign out
+            Firebase.auth.signOut()
+            startActivity(Intent(activity, MainActivity::class.java)) //go back to sign up/login activity
+        }
 
         return root
     }

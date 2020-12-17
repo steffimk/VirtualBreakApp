9e23955e7a5e964f1dd0dfa1cf31f8370453fa21
Jenny Phu <phuje@cip.ifi.lmu.de>
2020-12-17 22:38:16 +0100

    Profile fragment first structure with simple status spinner

--- app/src/main/java/com/example/virtualbreak/view/view_fragments/myprofile/MyProfileFragment.kt
+++ app/src/main/java/com/example/virtualbreak/view/view_fragments/myprofile/MyProfileFragment.kt
@@ -4,15 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.virtualbreak.R
+import com.example.virtualbreak.model.Status
+import kotlinx.android.synthetic.main.fragment_myprofile.*
 
-class MyProfileFragment : Fragment() {
+class MyProfileFragment : Fragment(), AdapterView.OnItemSelectedListener {
 
     private lateinit var myProfileViewModel: MyProfileViewModel
+
+    private var status_array = arrayOf(Status.STUDYING.dbStr, Status.BUSY.dbStr, Status.AVAILABLE.dbStr)
+    private lateinit var currentStatus: String
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,10 +30,38 @@ class MyProfileFragment : Fragment() {
         myProfileViewModel =
             ViewModelProvider(this).get(MyProfileViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_myprofile, container, false)
+
+        /*
         val textView: TextView = root.findViewById(R.id.text_gallery)
         myProfileViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
-        })
+        })*/
+
+        val spinner = root.findViewById<Spinner>(R.id.status_spinner)
+        spinner.setOnItemSelectedListener(this)
+
+        // Create an ArrayAdapter using a simple spinner layout and languages array
+        context?.let {
+            val aa = ArrayAdapter(
+                it,
+                android.R.layout.simple_spinner_item,
+                status_array
+            )
+            // Set layout to use when the list of choices appear
+            aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Set Adapter to Spinner
+            spinner.setAdapter(aa)
+        }
+
         return root
+    }
+
+    override fun onItemSelected(arg0: AdapterView<*>, arg1: View, position: Int, id: Long) {
+        //TODO set new status
+        currentStatus = status_array[position]
+    }
+
+    override fun onNothingSelected(arg0: AdapterView<*>) {
+
     }
 }

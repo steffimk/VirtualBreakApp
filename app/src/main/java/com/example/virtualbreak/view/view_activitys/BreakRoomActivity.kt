0979e875434c4e471ac528db9df7b7c94f8529bc
Manuela <ma.eska@campus.lmu.de>
2020-12-27 10:11:26 +0100

    navigation to room, checking for id (not yet loaded from db) added possibility to leave room and return to previous activity

--- app/src/main/java/com/example/virtualbreak/view/view_activitys/BreakRoomActivity.kt
+++ app/src/main/java/com/example/virtualbreak/view/view_activitys/BreakRoomActivity.kt
@@ -1,10 +1,13 @@
 package com.example.virtualbreak.view.view_activitys
 
+import android.content.Context
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.virtualbreak.R
+import com.example.virtualbreak.controller.communication.PullData
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_break_room.*
 
 
@@ -13,12 +16,28 @@ class BreakRoomActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_break_room)
 
+        val prefs = this.getSharedPreferences("com.example.virtualbreak", Context.MODE_PRIVATE)
+        val roomId = prefs.getString("com.example.virtualbreak.roomId", null)
+
+        /*
         val bundle: Bundle? = intent.extras
         if (bundle != null) {
             val roomId = bundle.getString("room_id")
 
             Toast.makeText(this, roomId, Toast.LENGTH_LONG).show()
-            // TODO get content from room with id roomId
+        }
+        */
+
+
+        if (roomId != null) {
+            // TODO: pull room from PullData (check for right function)
+            //PullData.attachListenerToRoom(roomId)
+
+            Toast.makeText(this, "not Null", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, R.string.something_wrong, Toast.LENGTH_LONG).show()
+            // ends activity and return to previous
+            finish()
         }
 
         // makes textview scrollable
@@ -29,6 +48,14 @@ class BreakRoomActivity : AppCompatActivity() {
 
             val input = chat_message_input.text
             input.clear()
+        }
+
+
+        // when leaving a room remove the roomId from preferences because it's not needed anymore and ends this activity
+        leave_room_button.setOnClickListener {
+            prefs.edit().remove("com.example.virtualbreak.roomId").apply()
+            // TODO: do we have to stop listener?
+            finish()
         }
     }
 }

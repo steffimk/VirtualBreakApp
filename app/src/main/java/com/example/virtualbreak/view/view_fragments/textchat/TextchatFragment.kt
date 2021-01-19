6fa4dee110f7ec1fc48ddf80800be8b721524d94
Manuela <ma.eska@campus.lmu.de>
2021-01-19 14:31:45 +0100

    first try making chat (breakroom content) as fragment

---
+++ app/src/main/java/com/example/virtualbreak/view/view_fragments/textchat/TextchatFragment.kt
@@ -0,0 +1,142 @@
+package com.example.virtualbreak.view.view_fragments.textchat
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.virtualbreak.R
+import com.example.virtualbreak.controller.Constants
+import com.example.virtualbreak.controller.SharedPrefManager
+import com.example.virtualbreak.controller.adapters.ChatAdapter
+import com.example.virtualbreak.controller.communication.PushData
+import com.example.virtualbreak.model.Message
+import com.example.virtualbreak.model.Room
+import com.example.virtualbreak.model.User
+import com.example.virtualbreak.view.view_activitys.breakroom.BreakRoomViewModel
+import com.example.virtualbreak.view.view_activitys.breakroom.BreakRoomViewModelFactory
+import kotlinx.android.synthetic.main.textchat_fragment.*
+
+//class TextchatFragment(private var userName: String?) : Fragment() {
+class TextchatFragment() : Fragment() {
+
+    private val TAG = "TextchatFragment"
+
+    //private lateinit var viewModel: TextchatViewModel
+    //private val viewModel = ViewModelProvider(this).get(TextchatViewModel::class.java)
+    private val viewModel: TextchatViewModel by viewModels{
+        TextchatViewModelFactory(
+            SharedPrefManager.instance.getRoomId() ?: ""
+        )
+    }
+
+    private var room: Room? = null
+    private val roomId: String? =
+        com.example.virtualbreak.controller.SharedPrefManager.instance.getRoomId()
+    private val userName: String? = SharedPrefManager.instance.getUserName()
+    private var chatAdapter: ChatAdapter? = null
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.textchat_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var defaultMessages: MutableList<Message> = ArrayList()
+        var defaultM = Message("default", "Keine Nachricht", Constants.DEFAULT_TIME)
+        defaultMessages.add(defaultM)
+
+        var layoutManager = LinearLayoutManager(context)
+        layoutManager.setStackFromEnd(true)
+        //var chatMessageRecyclerView = root.findViewById(chat_messages_recycler_view)
+        //Log.i(TAG, "recyclerView " + chatMessageRecyclerView)
+        chat_messages_recycler_view.layoutManager = layoutManager
+
+
+        chatAdapter = context?.let {
+            ChatAdapter(
+                it,
+                defaultMessages,
+                SharedPrefManager.instance.getRoomUsersHashmap()
+            )
+        }
+        chat_messages_recycler_view.adapter = chatAdapter
+        chatAdapter?.let {
+            chat_messages_recycler_view.smoothScrollToPosition(it.itemCount)
+        }
+
+        /*viewModel.getUser().observe(viewLifecycleOwner, Observer<User> { observedUser ->
+        if (observedUser != null) {
+            userName = observedUser.username
+        }})*/
+
+
+        viewModel.getRoom().observe(viewLifecycleOwner, Observer<Room>
+        { observedRoom ->
+
+            room = observedRoom
+            //context?.let { viewModel.loadUsersOfRoom(it) }
+
+            Log.d(TAG, "Observed room: $observedRoom")
+            if (observedRoom != null && observedRoom.messages != null && observedRoom.messages.isNotEmpty()) {
+                val messages = observedRoom.messages
+                var messagesList = ArrayList(messages.values)
+                messagesList.sortBy { it.timestamp }
+                Log.i(TAG, "messagesList: $messages")
+                //chat_messages_recycler_view.adapter = context?.let { ChatAdapter(it, messagesList, SharedPrefManager.instance.getRoomUsersHashmap()) }
+                if (chatAdapter == null) {
+                    chatAdapter = context?.let {
+                        ChatAdapter(
+                            it,
+                            messagesList,
+                            SharedPrefManager.instance.getRoomUsersHashmap()
+                        )
+                    }
+                    chat_messages_recycler_view.adapter = chatAdapter
+                } else {
+                    chatAdapter?.updateData(
+                        messagesList,
+                        SharedPrefManager.instance.getRoomUsersHashmap()
+                    )
+                }
+                chatAdapter?.let {
+                    chat_messages_recycler_view.smoothScrollToPosition(it.itemCount)
+                }
+            }
+        })
+
+        send_message_button.setOnClickListener {
+            val input = chat_message_input.text
+            val message = input.toString()
+            if (!message.isEmpty()) {
+                if (roomId != null) {
+                    PushData.sendMessage(roomId, message)
+                }
+            } else {
+                Toast.makeText(
+                    context, R.string.toast_enter_message,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            input.clear()
+        }
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+}
---
+++ app/src/main/java/com/example/virtualbreak/view/view_fragments/textchat/TextchatViewModel.kt
@@ -0,0 +1,99 @@
+package com.example.virtualbreak.view.view_fragments.textchat
+
+import android.content.Context
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.virtualbreak.controller.Constants
+import com.example.virtualbreak.controller.SharedPrefManager
+import com.example.virtualbreak.controller.communication.PullData
+import com.example.virtualbreak.model.Room
+import com.example.virtualbreak.model.User
+import com.example.virtualbreak.view.view_activitys.breakroom.BreakRoomViewModel
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.getValue
+import com.google.gson.Gson
+
+class TextchatViewModel(private val roomId: String) : ViewModel() {
+    private val TAG = "TextchatViewModel"
+
+    private val room : MutableLiveData<Room> = object : MutableLiveData<Room>() {
+        private val queryRoom = PullData.database.child(Constants.DATABASE_CHILD_ROOMS).child(roomId)
+
+        override fun onActive() {
+            super.onActive()
+            queryRoom.addValueEventListener(roomValueEventListener)
+        }
+
+        override fun onInactive() {
+            super.onInactive()
+            queryRoom.removeEventListener(roomValueEventListener)
+        }
+    }
+
+    fun getRoom(): LiveData<Room> {
+        return room
+    }
+
+    private val roomValueEventListener = object : ValueEventListener {
+        override fun onDataChange(dataSnapshot: DataSnapshot) {
+            val pulledRoom = dataSnapshot.getValue<Room>()
+            Log.d(TAG, "Pulled Room $pulledRoom")
+
+            room.value = pulledRoom
+        }
+
+        override fun onCancelled(databaseError: DatabaseError) {
+            Log.d(TAG, databaseError.message)
+        }
+    }
+
+    fun loadUsersOfRoom(context: Context){
+        val currentRoom = room.value
+        val users = currentRoom?.users
+
+        var usersOfRoom : HashMap<String,String> = HashMap()
+
+        Log.d(TAG, "loadUsersOfRoom")
+
+        val valueEventListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val user = dataSnapshot.getValue(User::class.java)
+                val name = user!!.username
+                Log.d(TAG, "User added: "+name)
+                usersOfRoom.put(dataSnapshot.key.toString(), name)
+
+                //convert to string using gson
+                val gson = Gson()
+                val hashMapString = gson.toJson(usersOfRoom)
+
+                SharedPrefManager.instance.removeRoomUsers()
+                SharedPrefManager.instance.saveRoomUsers(hashMapString)
+                //save hashmap in shared prefs
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.d(TAG, databaseError.message)
+            }
+        }
+
+        if (users != null) {
+            for(u in users){
+                PullData.database.child(Constants.DATABASE_CHILD_USERS).child(u.key).addListenerForSingleValueEvent(valueEventListener)
+            }
+        }
+    }
+}
+
+class TextchatViewModelFactory(private val roomId: String) :
+    ViewModelProvider.Factory {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return TextchatViewModel(roomId) as T
+    }
+
+}

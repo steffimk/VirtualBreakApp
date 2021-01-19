4bb67a9fa1eda2a1d039a8fd55d1943681b8cd49
Steffi <stefanie.m.kloss@t-online.de>
2021-01-19 15:58:57 +0100

    Moved addFCMTokenListener to NavigationDrawerActivity

--- app/src/main/java/com/example/virtualbreak/view/view_activitys/MainActivity.kt
+++ app/src/main/java/com/example/virtualbreak/view/view_activitys/MainActivity.kt
@@ -25,8 +25,6 @@ class MainActivity : AppCompatActivity() {
 
         SharedPrefManager.instance.init(applicationContext)
 
-        this.addFCMTokenListener()
-
         // If user is logged in, redirect to navigation drawer activity
         if (Firebase.auth.currentUser != null) {
             // Save userId in shared preferences
@@ -46,27 +44,6 @@ class MainActivity : AppCompatActivity() {
         loginBtn.setOnClickListener {
             startActivity(Intent(this, LogInActivity::class.java))
         }
-
-    }
-
-    /**
-     * Saves the Firebase Cloud Messaging Token in the database.
-     */
-    private fun addFCMTokenListener() {
-
-        Log.d(TAG, "addFCMTokenListener function called")
-
-        Firebase.messaging.token.addOnCompleteListener(OnCompleteListener { task ->
-            if (!task.isSuccessful) {
-                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
-                return@OnCompleteListener
-            }
-            val token = task.result
-            if (token != null) {
-                PushData.setFcmToken(token)
-                Log.d(TAG, "Saved fcm token $token")
-            }
-        })
 
     }
 

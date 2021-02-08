0913c28cf0acb5ee964b549e4f5cd34284360401
Manuela <ma.eska@campus.lmu.de>
2021-02-08 18:00:58 +0100

    checking friends already in group

--- app/src/main/java/com/example/virtualbreak/view/view_fragments/singlegroup/AddMemberToGroupFragment.kt
+++ app/src/main/java/com/example/virtualbreak/view/view_fragments/singlegroup/AddMemberToGroupFragment.kt
@@ -2,24 +2,20 @@ package com.example.virtualbreak.view.view_fragments.singlegroup
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.navigation.findNavController
 import com.example.virtualbreak.R
 import com.example.virtualbreak.controller.Constants
 import com.example.virtualbreak.controller.adapters.groupsfriends.SearchFriendListAdapter
 import com.example.virtualbreak.controller.communication.FCMService
 import com.example.virtualbreak.controller.communication.PushData
-import com.example.virtualbreak.model.NotificationBody
-import com.example.virtualbreak.model.NotificationData
-import com.example.virtualbreak.model.PushNotification
-import com.example.virtualbreak.model.User
+import com.example.virtualbreak.model.*
 import com.example.virtualbreak.view.view_fragments.groupsfriends.GroupsViewModel
 import kotlinx.android.synthetic.main.fragment_add_member_to_group.*
 
@@ -59,60 +55,103 @@ class AddMemberToGroupFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        adapter = SearchFriendListAdapter(ArrayList(), context)
-        //select_friends_recylerlist.adapter = SearchFriendListAdapter(friends, context)
-        add_select_friends_recylerlist.adapter = adapter
+        groupId?.let {
+            val singleGroupViewModel: SingleGroupViewModel by viewModels {
+                SingleGroupViewModelFactory(
+                    it
+                )
+            }
 
-        //for faster image loadig
-        add_select_friends_recylerlist.setHasFixedSize(true)
-        add_select_friends_recylerlist.setItemViewCacheSize(20)
+            adapter = SearchFriendListAdapter(ArrayList(), context)
+            add_select_friends_recylerlist.adapter = adapter
 
-        groupsFriendsViewModel.getFriends().observe(viewLifecycleOwner, Observer<HashMap<String, User>> { friendsMap ->
-            Log.d(TAG, "Observed Friends $friendsMap")
-            if (friendsMap != null){
+            //for faster image loadig
+            add_select_friends_recylerlist.setHasFixedSize(true)
+            add_select_friends_recylerlist.setItemViewCacheSize(20)
 
-                adapter.updateData(ArrayList(friendsMap.values))
-                //adapter = SearchFriendListAdapter(ArrayList(friendsMap.values), context)
-                add_select_friends_recylerlist.adapter = adapter
+            val usersOfGroup = singleGroupViewModel.getGroupUsers()
 
-                //Set the clicklistener to select friends and recive selected friends ids
-                adapter.setOnItemClickListener(object : SearchFriendListAdapter.OnItemClickListener{
-                    override fun onItemClick(friend: User) {
-                        if (friend.isSelected && !selectFriendsIds.contains(friend.uid)){
-                            selectFriendsIds.add(friend.uid)
-                        }else{
-                            selectFriendsIds.remove(friend.uid)
-                        }
-                        //adapter.notifyDataSetChanged()
-                        Log.d(TAG, "selectedFriends $selectFriendsIds")
+            var groupName = ""
+            singleGroupViewModel.getCurrentGroup()
+                .observe(viewLifecycleOwner, Observer<Group?> { currentGroup ->
+                    if (currentGroup != null) {
+                        groupName = currentGroup.description
                     }
                 })
-            }
-        })
 
-        add_member_button.setOnClickListener {
-            if(selectFriendsIds.isNotEmpty()){
-                this.joinGroup(selectFriendsIds.toTypedArray())
-                view.findNavController().navigate(R.id.action_addGroupFragment_to_navHome)
-            }else{
-                Toast.makeText(activity, getString(R.string.Error_no_friends), Toast.LENGTH_SHORT).show()
+            groupsFriendsViewModel.getFriends()
+                .observe(viewLifecycleOwner, Observer<HashMap<String, User>> { friendsMap ->
+                    Log.d(TAG, "Observed Friends $friendsMap")
+                    if (friendsMap != null) {
+
+                        var notAddedFriends: HashMap<String, User> = HashMap()
+
+                        usersOfGroup.observe(
+                            viewLifecycleOwner,
+                            Observer<HashMap<String, User>> { usersMap ->
+                                friendsMap.forEach() { (key, user) ->
+                                    if (usersMap.contains(key)) {
+                                        // already in list
+                                    } else {
+                                        notAddedFriends.put(key, user)
+                                    }
+                                }
+                            })
+
+                        if (notAddedFriends.isEmpty()) {
+                            adapter.updateData(ArrayList(friendsMap.values))
+                        } else {
+                            adapter.updateData(ArrayList(notAddedFriends.values))
+                        }
+
+                        //adapter = SearchFriendListAdapter(ArrayList(friendsMap.values), context)
+                        add_select_friends_recylerlist.adapter = adapter
+
+                        //Set the clicklistener to select friends and recive selected friends ids
+                        adapter.setOnItemClickListener(object :
+                            SearchFriendListAdapter.OnItemClickListener {
+                            override fun onItemClick(friend: User) {
+                                if (friend.isSelected && !selectFriendsIds.contains(friend.uid)) {
+                                    selectFriendsIds.add(friend.uid)
+                                } else {
+                                    selectFriendsIds.remove(friend.uid)
+                                }
+                                //adapter.notifyDataSetChanged()
+                                Log.d(TAG, "selectedFriends $selectFriendsIds")
+                            }
+                        })
+                    }
+                })
+
+            add_member_button.setOnClickListener {
+                if (selectFriendsIds.isNotEmpty()) {
+                    this.joinGroup(selectFriendsIds.toTypedArray(), groupName)
+                } else {
+                    Toast.makeText(
+                        activity,
+                        getString(R.string.Error_no_friends),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                parentFragmentManager.setFragmentResult(
+                    Constants.REQUEST_KEY_ADD_MEMBER,
+                    bundleOf(Constants.BUNDLE_KEY_ADD_MEMBER to false)
+                )
             }
-            parentFragmentManager.setFragmentResult(
-                Constants.REQUEST_KEY_ADD_MEMBER,
-                bundleOf(Constants.BUNDLE_KEY_ADD_MEMBER to false)
-            )
         }
     }
 
-    private fun joinGroup(friendIds: Array<String>) {
+    private fun joinGroup(friendIds: Array<String>, groupName: String) {
         friendIds.forEach { id ->
             PushData.joinGroup(groupId!!, id)
             val title = "Neue Gruppe"
-            //val message = "Du wurdest zu der Gruppe \"$groupName\" hinzugefügt"
-            val message = "Du wurdest zu der Gruppe \"$groupId\" hinzugefügt"
+            var message = "Du wurdest zu der Gruppe \"$groupId\" hinzugefügt"
+            if (groupName != "") {
+                message = "Du wurdest zu der Gruppe \"$groupName\" hinzugefügt"
+            }
             val recipientToken = groupsFriendsViewModel.getFriends().value?.get(id)?.fcmToken
             Log.d(TAG, "FCMToken: $recipientToken")
-            if(recipientToken != null) {
+            if (recipientToken != null) {
                 PushNotification(
                     NotificationData(title, message),
                     NotificationBody(title, message),
@@ -127,22 +166,19 @@ class AddMemberToGroupFragment : Fragment() {
 
     companion object {
         /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
+         * Creating a new instance of this fragment using the provided parameters.
          *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment addMemberToGroupFragment.
+         * @param groupId Id the current group
+         * @return A new instance of fragment AddMemberToGroupFragment.
          */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(groupId : String) =
+        fun newInstance(groupId: String) =
             AddMemberToGroupFragment()
                 .apply {
-                arguments = Bundle().apply {
-                    putString(GROUP_ID, groupId)
+                    arguments = Bundle().apply {
+                        putString(GROUP_ID, groupId)
+                    }
+                    Log.i(TAG, "created")
                 }
-                    Log.i(TAG , "created")
-            }
     }
 }
